3143e15fca3e0a96ef5f4e896ae48a470d387149
Brayden Langley <brayden167@gmail.com>
2025-06-25 13:13:21 -0700

    Enable urlOpener to support links

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,6 @@ import { onWalletReady } from './onWalletReady'
 import ErrorBoundary from './ErrorBoundary'
 import { tauriFunctions } from './tauriFunctions'
 import packageJson from '../package.json'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-
-// Define a theme that includes the custom structure expected by the components
-const theme = createTheme({
-  // Add any standard theme overrides here
-  templates: {
-    page_wrap: {
-      // This is needed to prevent the error, define specific styles here if required
-    }
-  }
-})
 
 // Create the root and render:
 const rootElement = document.getElementById('root')
@@ -26,28 +15,26 @@ if (rootElement) {
 
   root.render(
     <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <ErrorBoundary>
-          <UserInterface
-            onWalletReady={onWalletReady}
-            nativeHandlers={tauriFunctions}
-            appVersion={packageJson.version}
-            appName="Metanet Desktop"
-          />
-        </ErrorBoundary>
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
+      <ErrorBoundary>
+        <UserInterface
+          onWalletReady={onWalletReady}
+          nativeHandlers={tauriFunctions}
+          appVersion={packageJson.version}
+          appName="Metanet Desktop"
         />
-      </ThemeProvider>
+      </ErrorBoundary>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light" // TODO: Make this dynamic based on system theme
+      />
     </React.StrictMode>
   )
 }
